0eda2897776514b6ba28b1967656af10fd9b728c
DariaAzanovich <azanovich.dasha@gmail.com>
2021-08-07 20:56:35 +0300

    feat: search window revision

--- workflow/landing-page/sources/sctript.js
+++ workflow/landing-page/sources/sctript.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function ready() {
     document.body.classList.remove('preload');
 });
+
+const searchBtn = document.querySelector('.search-btn');
+const searchWindow = document.querySelector('.search-window');
+const closeBtn = document.querySelector('.close-btn');
+
+searchBtn.addEventListener('click', function () {
+    searchWindow.style.visibility = 'visible';
+})
+
+closeBtn.addEventListener('click', function () {
+    searchWindow.style.visibility = 'hidden';
+})
