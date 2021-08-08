7a41e8595cbd3b2bb1ed0465fa481011339f4d7f
DariaAzanovich <azanovich.dasha@gmail.com>
2021-08-08 10:41:03 +0300

    feat: add burder menu & click handler

--- workflow/landing-page/sources/sctript.js
+++ workflow/landing-page/sources/sctript.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function ready() {
 const searchBtn = document.querySelector('.search-btn');
 const searchWindow = document.querySelector('.search-window');
 const closeBtn = document.querySelector('.close-btn');
+const burgerBtn = document.querySelector('.nav__burger-menu');
+const dropdownMenu = document.querySelector('.dropdown-menu')
 
 searchBtn.addEventListener('click', function () {
     searchWindow.classList.remove('closed-search-window');
@@ -12,4 +14,8 @@ searchBtn.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
     searchWindow.classList.add('closed-search-window');
+})
+
+burgerBtn.addEventListener('click', function() {
+    dropdownMenu.classList.toggle('closed-dropdown-menu');
 })
