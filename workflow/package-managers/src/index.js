a9c1edd158bf2871ccde1c4bd59512a11d120b88
DariaAzanovich <azanovich.dasha@gmail.com>
2021-08-30 16:50:00 +0300

    feat: webpack, loaders, plugins, bundle, run cmd

--- workflow/package-managers/src/index.js
+++ workflow/package-managers/src/index.js
@@ -1,3 +1,6 @@
+import logo from './img/magic-box.png';
+import './styles/style.css';
+
 const NUMBER_COLOR = 'red';
 const STRING_COLOR = 'green';
 const BOOLEAN_COLOR = 'purple';
@@ -13,31 +16,28 @@ const treeBtn = document.querySelector('.tree-btn');
 const dataArea = document.getElementById('d-area');
 const tree = document.querySelector('.tree');
 const jsonFormatExmpl = document.querySelector('.json-format');
-const jsonData = document.querySelector('.json-data');
 const errMessage = document.querySelector('.error-message');
 
-
-/*--------------------- Functions ------------------*/
+/* --------------------- Functions ------------------ */
 
 function typeBrackets(data) {
     let keyBrackets = '';
 
-    if(Array.isArray(data)) {
+    if (Array.isArray(data)) {
         keyBrackets = ' []';
     } else {
         keyBrackets = ' {}';
     }
-    
+
     return keyBrackets;
 }
 
-
 function setSpanColorizedData(span, data) {
-    switch(typeof data) {
-        case 'number':  
+    switch (typeof data) {
+        case 'number':
             span.style.color = NUMBER_COLOR;
             break;
-        case 'string': 
+        case 'string':
             span.style.color = STRING_COLOR;
             break;
         case 'boolean':
@@ -46,6 +46,25 @@ function setSpanColorizedData(span, data) {
     }
 
     span.innerHTML = data;
+
+    return span;
+}
+
+function collapseExpand(event) {
+    event.stopPropagation();
+
+    const ulSibl = event.target.nextSibling;
+    ulSibl.classList.toggle('collapsed');
+
+    event.target.classList.toggle('active');
+}
+
+function createClickableSpan(content) {
+    const span = document.createElement('span');
+    span.classList.add('clickable');
+    span.classList.add('active');
+    span.addEventListener('click', collapseExpand);
+    span.innerHTML = content;
 
     return span;
 }
@@ -61,8 +80,8 @@ function createTreeDom(obj) {
         const li = document.createElement('li');
         const span = document.createElement('span');
 
-        if(typeof obj[key] !== 'object') {
-            if(!Array.isArray(obj)) {
+        if (typeof obj[key] !== 'object') {
+            if (!Array.isArray(obj)) {
                 li.innerHTML = key + ': ';
             }
             li.appendChild(setSpanColorizedData(span, obj[key]));
@@ -77,33 +96,14 @@ function createTreeDom(obj) {
 
         ul.append(li);
     }
+
     return ul;
-}
-
-function collapseExpand(event) {
-    event.stopPropagation();
-
-    const ulSibl = event.target.nextSibling;
-    ulSibl.classList.toggle('collapsed');
-
-    event.target.classList.toggle("active");
-}
-
-function createClickableSpan(content) {
-    const span = document.createElement('span');
-    span.classList.add('clickable');
-    span.classList.add('active');
-    span.addEventListener('click', collapseExpand);
-    span.innerHTML = content;
-
-    return span;
 }
 
 function appendTree(container, obj) {
     container.appendChild(createTreeDom(obj));
     container.firstChild.insertAdjacentElement('beforebegin', createClickableSpan('JSON data' + typeBrackets(obj)));
 }
-
 
 function jsonDataHandling() {
     const data = dataArea.value;
@@ -114,14 +114,13 @@ function jsonDataHandling() {
         const parsedJson = JSON.parse(data);
         const span = document.createElement('span');
 
-        if(typeof parsedJson === 'object') {
+        if (typeof parsedJson === 'object') {
             appendTree(tree, parsedJson);
         } else {
             tree.appendChild(setSpanColorizedData(span, parsedJson));
         }
-
-    } catch(err) {
-        if (err.name == "SyntaxError") {
+    } catch (err) {
+        if (err.name === 'SyntaxError') {
             const message = `Your code doesn\'t match the JSON format! Error: ${err.message}`;
 
             errMessage.innerHTML = message;
@@ -131,12 +130,11 @@ function jsonDataHandling() {
     }
 }
 
-/*----------------- Event listeners -----------------*/
+/* ----------------- Event listeners ----------------- */
 
 document.addEventListener('DOMContentLoaded', () => {
     dataArea.value = EXAMP_DATA;
     jsonFormatExmpl.innerHTML = JSON_FORMAT;
-})
+});
 
-
-treeBtn.addEventListener('click', jsonDataHandling);  
+treeBtn.addEventListener('click', jsonDataHandling);
