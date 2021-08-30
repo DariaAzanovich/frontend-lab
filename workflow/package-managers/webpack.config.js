d8ea67045e0d232b3ba2a28dfbc3972eda9bc015
DariaAzanovich <azanovich.dasha@gmail.com>
2021-08-30 20:35:46 +0300

    feat: add MiniCssExtractPlugin, minify html

--- workflow/package-managers/webpack.config.js
+++ workflow/package-managers/webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -14,11 +18,15 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
@@ -28,13 +36,21 @@ module.exports = {
                     to: path.resolve(__dirname, 'dist')
                 }
             ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
         })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {}
+                },
+                'css-loader']
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
