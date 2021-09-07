4082c9902a5da5586edc4d1d8fa2df3b21b3f1c8
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-07 19:31:40 +0300

    feat: add quotes arr

--- workflow/react-project/src/components/Quote.js
+++ workflow/react-project/src/components/Quote.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export const Quite = () => {
+const qoutes = [
+    'Always remember, that I have taken more out of alcohol than alcohol has taken out of me.',
+    'Alcohol, what\'s that? It\'s not in my vodkabulary, but let me check in whiskypedia.',
+    'Not to get technical, But according to chemistry  ALCOHOL is a solution.',
+    'Always buy a bigger bottle than you think You’ll need. Better to be safe than sober.',
+    'In wine there is wisdom, in beer there is Freedom, in water there is bacteria.'
+];
+
+export const Quote = () => {
     return (
         <div></div>
     );
