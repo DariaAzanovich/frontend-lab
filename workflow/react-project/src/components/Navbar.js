74ee90e1846751f3f30a6b32234bf95a2d8e4ae0
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-08 14:25:54 +0300

    fix: del modal open on icon

--- workflow/react-project/src/components/Navbar.js
+++ workflow/react-project/src/components/Navbar.js
@@ -13,9 +13,6 @@ export const Navbar = ({ closeModal }) => {
                         icon={faCocktail} 
                         size="3x"
                         className="navbar-logo"
-                        onClick={() => {
-                            closeModal(true)
-                        }}
                     />
                     <div className="navbar-title">
                         Coctail App
