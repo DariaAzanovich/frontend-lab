0cd740f84f025b3c0e37fe16ef50b87ec3e6a448
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-13 21:16:01 +0300

    feat: modal style, homePage imports

--- workflow/react-project/src/pages/Home.js
+++ workflow/react-project/src/pages/Home.js
@@ -1,15 +1,21 @@
 import './Home.css';
 import './pagesMedia.css'
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Modal from '../components/Modal';
 import QuotesCarousel from '../components/QuotesCarousel';
+import { fetchRandomCocktail } from '../redux/actions';
+import RandomCocktail from '../components/RandomCocktail';
 
 export const Home = () => {
     const [openModal, setOpenModal] = useState(false);
+    const dispatch = useDispatch();
+    // const cocktail = useSelector(state => state.posts.cocktail);
+
     return (
         <>
             <div className="wrap">
-                {openModal && <Modal modalState={setOpenModal}/>}
+                {openModal && <Modal modalState={setOpenModal} content={<RandomCocktail/>}/>}
 
                 <h1 className="homepage-title">Cocktail App</h1>
 
@@ -21,7 +27,8 @@ export const Home = () => {
                             alt="Green cocktail" 
                             src="./green-cocktail.png"
                             onClick={() => {
-                                setOpenModal(true)
+                                setOpenModal(true);
+                                fetchRandomCocktail()(dispatch);
                             }}
                         />
                         <p className="content-prompt">Press on glass to get a random cocktail</p>
