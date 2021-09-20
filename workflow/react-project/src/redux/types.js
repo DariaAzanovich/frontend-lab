8d3b145a5d62f01b5b6f9b794e7dc8ec4e27789a
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-20 15:11:31 +0300

    feat: add types

--- workflow/react-project/src/redux/types.js
+++ workflow/react-project/src/redux/types.js
@@ -1,3 +1,15 @@
+/*----------------- Random Cocktail ----------------*/
 export const FETCH_COCKTAIL_SUCCESS = 'FETCH_COCKTAIL_SUCCESS';
 export const FETCH_LOADING  = 'FETCH_LOADING';
 export const FETCH_COCKTAIL_FAIL = 'FETCH_COCKTAIL_FAIL';
+
+/*----------------- Authentication ----------------*/
+export const AUTHENTICATION_STARTED = 'AUTHENTICATION_STARTED';
+
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAIL= 'SIGN_IN_FAIL';
+
+export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
+export const REGISTRATION_FAIL = 'REGISTRATION_FAIL';
+
+export const ADD_USERNAME = 'ADD_USERNAME';
