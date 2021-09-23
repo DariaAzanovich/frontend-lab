e2fa552be320c5ea4d024343061dd34c9e9c3eb2
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-23 12:54:59 +0300

    refactor: del unused var-s

--- workflow/react-project/src/components/Authentication.js
+++ workflow/react-project/src/components/Authentication.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Loader from './Loader';
 import './Authentication.css';
