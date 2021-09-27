808c6b8f70fe74fe75c5f8ec8bac5e7a0f0c3e23
DariaAzanovich <azanovich.dasha@gmail.com>
2021-09-27 17:33:16 +0300

    del log

--- workflow/react-project/src/pages/SearchCocktail.js
+++ workflow/react-project/src/pages/SearchCocktail.js
@@ -24,8 +24,6 @@ const SearchCocktail = (props) => {
             props.fetchSearchCocktails(search);
         }, 1000)
     );
-
-    console.log(props.cocktails);
     
     useEffect(() => {
         if (search) {
